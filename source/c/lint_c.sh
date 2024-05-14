a78b3865468397a29932b25530edd785fc90fdc9
tomhosker <tomdothosker@gmail.com>
2024-05-14 15:21:31 +0100

    Added Valgrind.

--- source/c/lint_c.sh
+++ source/c/lint_c.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-### This script lints all the C code in this directory.
+### This script lints all the C code in this directory, looking for both style
+### issues and memory leaks.
 
 # Local constants.
 PATH_TO_HERE=$(dirname $0)
-LINTING_ARGS="-lgmp -Wall -Wextra -Wfloat-equal -pedantic -ansi -O2"
+LINTING_ARGS="-Wall -Wextra -Wfloat-equal -pedantic -ansi -O2"
 TEMP_FN="temp.out"
 
 # Let's get cracking...
-for lib in "*/"; do
+for lib in *; do
     path_to_main="$lib/main.c"
     if [ -f $path_to_main ]; then
+        echo "Checking $(realpath $lib)..."
         if gcc $path_to_main -lgmp $LINTING_ARGS -o $TEMP_FN; then
+            valgrind --leak-check=yes ./$TEMP_FN
             rm $TEMP_FN
         fi
     fi
